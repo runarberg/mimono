daeb66b55e5a277529f4a265fbb101dd1b869755
runarberg <runarberg@zoho.com>
2013-07-12 17:10:01 +0000

    preview.command replaced with preview.py

--- preview.command
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd "`dirname "$0"`"
-
-pelican -s devconf.py
-
-cd webpage-dev
-python -m "SimpleHTTPServer"
--- preview.py
+++ preview.py
@@ -6,12 +6,25 @@ A read-eval-print-loop to view the development version of the webpage
 
 import os
 import subprocess
+
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+from BaseHTTPServer import HTTPServer
 from subprocess import PIPE
 
-subprocess.Popen(['pelican', '-s', 'devconf.py', '-r'],
-                 stdin=PIPE, stdout=PIPE, stderr=PIPE)
+def run_server():
+    port = 8000
+    httpd = HTTPServer(('', port), SimpleHTTPRequestHandler)
+    print "serving at port ", port
+    httpd.serve_forever()
 
-os.chdir('webpage-dev')
-subprocess.Popen(['python', '-m', 'SimpleHTTPServer'])
+    
+if __name__ in "__main__":
+    subprocess.Popen(['pelican', '-s', 'devconf.py', '-r'],
+                     stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-subprocess.call(['open', 'http://127.0.0.1:8000'])
+    # TODO: Have to make replace the 'open' command with a UNIX neutral command
+    subprocess.call(['open', 'http://127.0.0.1:8000'])
+
+    os.chdir('webpage-dev')
+    run_server()
+
